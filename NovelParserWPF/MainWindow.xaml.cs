9a163f4980b7378f3f3c0568ff2f8effd8e1d2c7
Relorer <Y.tm@bk.ru>
2022-09-18 15:36:05 +0300

    remove Test

--- NovelParserWPF/MainWindow.xaml.cs
+++ NovelParserWPF/MainWindow.xaml.cs
@@ -1,18 +1,4 @@
-﻿using NovelParserBLL;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace NovelParserWPF
 {
@@ -24,9 +10,6 @@ namespace NovelParserWPF
         public MainWindow()
         {
             InitializeComponent();
-
-
-            new Test().StartTest();
         }
     }
 }
